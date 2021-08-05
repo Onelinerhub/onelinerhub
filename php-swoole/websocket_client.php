8023c44392407ad27c4cd96ddd854b361b30620b
Joe Markberg <inonunicorn@gmail.com>
2021-08-05 15:07:44 +0300

    Create websocket_client.php

---
+++ php-swoole/websocket_client.php
@@ -0,0 +1,29 @@
+# Websocket example
+
+```php
+include 'client.php';
+$w = new WebSocketClient('127.0.0.1', 9504);
+$w->connect();
+$w->send('test');
+echo $w->recv();
+
+```
+
+- client.php - include example [websocket implementation](https://github.com/nonunicorn/php-swoole-websocket-client/blob/main/client.php) based on Swoole
+- WebSocketClient - websocket wrapper class
+- '127.0.0.1', 9504 - [websocket server](/php-swoole/websocket_server) to connect to
+- connect() - connect to server
+- send('test') - send some message to server
+- echo $w->recv() - get response from server and print it
+
+## Example
+```php
+// ...
+$w->send('test');
+echo $w->recv();
+```
+```bash
+Hello, test
+```
+
+group: websocket
