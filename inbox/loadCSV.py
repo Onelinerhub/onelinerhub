d49b167b0a4bc6245385c8496e07274b949c58f0
Joe Markberg / code editor <inonunicorn@gmail.com>
2022-04-09 14:19:11 +0300

    Deployed
    https://onelinerhub.com/python-pandas/how-to-load-data-frame-from-csv

--- inbox/loadCSV.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-# Open the csv file
-fname = open('wholesale-trade-survey-dec-2021-quarter-csv.csv')
-# Reading the csv file using panda's read_csv method
-tradeSurvey = pd.read_csv(fname) 
-print(tradeSurvey)
