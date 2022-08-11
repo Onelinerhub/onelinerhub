0cebda4862e9702650c415a76b2e7ccda1d38cf7
Chief Editor / onelinerhub.com <inonunicorn@gmail.com>
2022-08-11 18:01:03 +0300

    Moved to production
    https://onelinerhub.com/nodejs/how-to-get-current-timestamp-in-seconds

--- inbox/timestamp.js
+++
@@ -1,7 +0,0 @@
-//Get Current Timestamp in Nodejs
-
-//current timestamp in milliseconds
-const currentTimeStamp = Date.now();
-
-//Current timestamp in seconds
-const currentTimeStampInSec = Math.floor(Date.now() / 1000);
